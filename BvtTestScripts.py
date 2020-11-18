fa3832d73afaba1a35eefa03d8e90317da33074e
Gokul <47682121+Goku747@users.noreply.github.com>
2020-11-18 17:24:19 +0530

    BVT test script added for Embedded device
    basic test cases for testing device functionality added

---
+++ BvtTestScripts.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+import pexpect
+import sys
+child = pexpect.spawn('sudo minicom -D /dev/ttyUSB0', env = {"TERM": "vt102"})
+child.sendline('')
+child.expect('root@imx8dx_ccu:~#')
+
+#Checking whether M4 is working
+print("**********Checking whether M4 is working**********")
+child.sendline('ls /dev/rpmsg*')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Checking Artifact Version
+print("**********Checking Artifact Version**********")
+child.sendline('cat /etc/mender/artifact_info')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Printing Log Messages
+print("**********Printing Log Messages**********")
+child.sendline('cat /logs/messages')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Checking Process Status
+print("**********Checking Process Status**********")
+child.sendline('ps | grep mbsa_start.sh')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Checking LTE connectivity
+print("**********Checking LTE connectivity**********")
+child.sendline('ping 172.217.17.68')
+child.expect('\r\n')
+child.sendcontrol('c')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-4]
+print('\n')
+
+#Checking Disk Space
+print("**********Checking Disk Space**********")
+child.sendline('df -h')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Checking Memory Usage Status
+print("**********Checking Memory Usage Status**********")
+child.sendline('top')
+child.sendline('')
+child.sendcontrol('c')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-4]
+print('\n')
+
+#Checking Ethernet
+print("**********Checking Ethernet**********")
+child.sendline('ifconfig eth0')
+child.expect('\r\n')
+child.expect('root@imx8dx_ccu:~#')
+print child.before[:-2]
+print('\n')
+
+#Terminating Minicom
+print("**********Terminating Minicom**********")
+child.terminate(force=True)
+sys.exit()
